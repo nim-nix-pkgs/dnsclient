21e5d16bd5105f953ff6ed86029f53cb13d20f82
hugosenari <hugosenari@gmail.com>
2022-03-12 02:43:53 -0300

    chore: re index dnsclient

---
+++ 0_0_3/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Simple DNS Client & Library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-dnsclient-0_0_3.flake = false;
+  inputs.src-dnsclient-0_0_3.owner = "ba0f3";
+  inputs.src-dnsclient-0_0_3.ref   = "refs/tags/0.0.3";
+  inputs.src-dnsclient-0_0_3.repo  = "dnsclient.nim";
+  inputs.src-dnsclient-0_0_3.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-dnsclient-0_0_3"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-dnsclient-0_0_3";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = ''Simple DNS Client & Library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."dnsclient-master".url = "path:./master";
+  inputs."dnsclient-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnsclient-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnsclient-0_0_1".url = "path:./0_0_1";
+  inputs."dnsclient-0_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnsclient-0_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnsclient-0_0_2".url = "path:./0_0_2";
+  inputs."dnsclient-0_0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnsclient-0_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnsclient-0_0_3".url = "path:./0_0_3";
+  inputs."dnsclient-0_0_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnsclient-0_0_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnsclient-0_1_0".url = "path:./0_1_0";
+  inputs."dnsclient-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnsclient-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnsclient-0_1_1".url = "path:./0_1_1";
+  inputs."dnsclient-0_1_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnsclient-0_1_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."dnsclient-0_1_2".url = "path:./0_1_2";
+  inputs."dnsclient-0_1_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dnsclient-0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
